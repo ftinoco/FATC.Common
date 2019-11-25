0764ad0497db49c88d8544c812b875358cb29480
ftinoco <fernandotinoco_45@hotmail.com>
2019-11-24 21:23:43 -0600

    Add pagination options (NuGet v1.0.4)

---
+++ FATC.Common/PagingOptions.cs
@@ -0,0 +1,24 @@
+﻿namespace FATC.Common
+{
+    public class PagingOptions
+    {
+        private const int maxPageSize = 100;
+
+        public bool IncludeCount { get; set; } = false;
+
+        public int? PageNumber { get; set; }
+
+        private int _pageSize = 50;
+        public int PageSize
+        {
+            get
+            {
+                return _pageSize;
+            }
+            set
+            {
+                _pageSize = (value < maxPageSize) ? value : maxPageSize;
+            }
+        }
+    }
+}
