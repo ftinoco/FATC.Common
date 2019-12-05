a227e0fa6962578a15cec60201af29500fd700b4
ftinoco <fernandotinoco_45@hotmail.com>
2019-12-04 23:23:19 -0600

    Add validation error class (NuGet 1.0.4.5)

---
+++ FATC.Common/Helpers/ValidationError.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace FATC.Common.Helpers
+{
+    public class ValidationError
+    {
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+
+        [JsonProperty(PropertyName = "description")]
+        public string Description { get; set; }
+    }
+}
