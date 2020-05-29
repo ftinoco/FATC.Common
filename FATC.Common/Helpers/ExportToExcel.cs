eb4275a858e3737b1a4323d93f9079f4be82c45e
ftinoco <fernandotinoco_45@hotmail.com>
2020-05-29 11:07:44 -0600

    Test generate byte array of excel file (Nugget 1.0.4.9)

---
+++ FATC.Common/Helpers/ExportToExcel.cs
@@ -0,0 +1,96 @@
+﻿using FATC.Common.Extensions;
+using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace FATC.Common.Helpers
+{
+    public class ExportToExcel<TEntity>
+    {
+        public static byte[] GenerateExcel(ReportEntity entity)
+        {
+            string tmpFile = $"{Path.GetDirectoryName(entity.TemplatePath)}\\{Guid.NewGuid()}_report.xlsx";
+            try
+            {
+                File.Copy(entity.TemplatePath, tmpFile, true);
+                var destFileInfo = new FileInfo(tmpFile);
+
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (FileStream fs = File.OpenRead(tmpFile))
+                    {
+                        using (ExcelPackage excelPackage = new ExcelPackage(fs))
+                        {
+                            ExcelWorkbook excelWorkBook = excelPackage.Workbook;
+                            ExcelWorksheet excelWorksheet = excelWorkBook.Worksheets.FirstOrDefault(x => x.Name.Equals(entity.SheetName));
+                            var tuple = CreateHeaderRow(excelWorksheet, entity.ExcelEntities);
+                            excelPackage.SaveAs(ms); // This is the important part.
+                        }
+
+                        ms.Position = 0;
+                        var result = ms.ToArray();
+                        ms.Flush();
+
+                        if (File.Exists(tmpFile))
+                            File.Delete(tmpFile);
+
+                        return result;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                if (File.Exists(tmpFile))
+                    File.Delete(tmpFile);
+                throw ex;
+            }
+        }
+
+        private static (uint index, List<TEntity> entities) CreateHeaderRow(ExcelWorksheet excelWorksheet, List<ExcelEntity> entities)
+        {
+            int columnIndex = 0;
+            uint indexHeader = 0;
+            List<TEntity> lst = new List<TEntity>();
+
+            foreach (var entity in entities)
+            {
+                if (entity.Value == null) continue;
+
+                if (entity.Value.IsList())
+                {
+                    lst = (List<TEntity>)entity.Value;
+
+                    //Row header = new Row();
+                    //header.RowIndex = entity.RowIndex;
+                    //indexHeader = entity.RowIndex;
+
+                    //var properties = typeof(TEntity).GetProperties();
+                    //foreach (var pro in properties)
+                    //{
+                    //    var displayAttribute = pro.GetCustomAttribute<DisplayAttribute>();
+                    //    if (displayAttribute != null)
+                    //    {
+                    //        Cell headerCell = CreateCell(displayAttribute.Name, true);
+                    //        headerCell.CellReference = entity.ColumnName;
+                    //        header.Append(headerCell);
+                    //        columnIndex++;
+                    //    }
+                    //}
+                    //sheetData.AppendChild(header);
+                }
+                else
+                {
+                    excelWorksheet.Cells[entity.RowIndex, entity.ColumnName].Value = entity.Value.ToString();
+                    //Cell cell = GetCell(worksheetPart.Worksheet, entity.ColumnName, entity.RowIndex);
+                    //cell.CellValue = new CellValue(entity.Value.ToString());
+                    //cell.DataType = new EnumValue<CellValues>(CellValues.Number);
+                }
+                // Save the worksheet.
+                //worksheetPart.Worksheet.Save();
+            }
+            return (indexHeader, lst);
+        } 
+    }
+}
---
+++ FATC.Common/ReportEntity.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace FATC.Common
+{
+    public class ReportEntity
+    {
+        public string TemplatePath { get; set; }
+
+        public string SheetName { get; set; }
+
+        public List<ExcelEntity> ExcelEntities { get; set; }
+
+    }
+
+    public class ExcelEntity
+    {
+        public int RowIndex { get; set; }
+
+        public int ColumnName { get; set; }
+
+        public object Value { get; set; }
+    }
+}
